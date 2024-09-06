76b6756987fea2d2c045b146ebf806854706e821
zerock <zk2020308a@gmail.com>
2024-09-06 17:05:51 +0900

    0906 - 10th

--- src/main/java/org/zerock/w2/controller/BoardController.java
+++ src/main/java/org/zerock/w2/controller/BoardController.java
@@ -3,13 +3,19 @@ package org.zerock.w2.controller;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.w2.dto.BoardRegisterDTO;
+import org.zerock.w2.dto.MemberDTO;
 import org.zerock.w2.service.BoardService;
 import org.zerock.w2.vo.PageRequest;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/board/")
@@ -20,7 +26,17 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("register")
-    public void register() {
+    public void register(Authentication authentication, Model model) {
+
+        log.info("register");
+
+        log.info(authentication);
+
+        log.info(authentication.getPrincipal());
+
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        log.info("-------------------------------");
 
     }
 
@@ -44,6 +60,7 @@ public class BoardController {
 
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("read/{bno}")
     public String read(@PathVariable("bno") Long bno, Model model ) {
 
